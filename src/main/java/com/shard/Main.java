5abff7498fe6039b626bca14795fcd3287a21d3a
JXS_CJL\bqmcj <Cjl>
2023-08-01 14:22:55 +0800

    init

---
+++ src/main/java/com/shard/Main.java
@@ -0,0 +1,12 @@
+package com.shard;
+
+/**
+ * @author chen_jinglong
+ * @description: ${TODO}
+ * @date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
